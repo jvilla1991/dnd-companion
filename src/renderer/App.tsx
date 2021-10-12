86e615d5e210fabaa03edcd6305d9dc95fa92a90
Jeremy Villa <villajeremy1991@gmail.com>
2021-10-12 10:57:00 -0400

    made login button

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -12,27 +12,12 @@ const Hello = () => {
       <h1>electron-react-boilerplate</h1>
       <div className="Hello">
         <a
-          href="https://electron-react-boilerplate.js.org/"
-          target="_blank"
-          rel="noreferrer"
-        >
-          <button type="button">
-            <span role="img" aria-label="books">
-              📚
-            </span>
-            Read our docs
-          </button>
-        </a>
-        <a
           href="https://github.com/sponsors/electron-react-boilerplate"
           target="_blank"
           rel="noreferrer"
         >
           <button type="button">
-            <span role="img" aria-label="books">
-              🙏
-            </span>
-            Donate
+            Login
           </button>
         </a>
       </div>
