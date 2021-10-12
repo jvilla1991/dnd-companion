e554c3ba550c5548fdc20e4adbfbca66038706dd
Jeremy Villa <villajeremy1991@gmail.com>
2021-10-12 11:28:55 -0400

    Made User Object for login

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -9,16 +9,14 @@ const Hello = () => {
       <div className="Hello">
         <img width="200px" alt="icon" src={icon} />
       </div>
-      <h1>electron-react-boilerplate</h1>
+      <h1>DnD Companion</h1>
       <div className="Hello">
         <a
           href="https://github.com/sponsors/electron-react-boilerplate"
           target="_blank"
           rel="noreferrer"
         >
-          <button type="button">
-            Login
-          </button>
+          <button type="button">Login</button>
         </a>
       </div>
     </div>
