aee02414d4740eefbce27112ee01b290a06941a6
Jeremy Villa <villajeremy1991@gmail.com>
2021-10-12 15:19:34 -0400

    Added Material UI package

--- src/renderer/App.tsx
+++ src/renderer/App.tsx
@@ -23,11 +23,32 @@ const Hello = () => {
   );
 };
 
+const Welcome = () => {
+  return (
+    <div>
+      <div className="Hello">
+        <img width="200px" alt="icon" src={icon} />
+      </div>
+      <h1>DnD Companion</h1>
+      <div className="Hello">
+        <a
+          href="https://github.com/sponsors/electron-react-boilerplate"
+          target="_blank"
+          rel="noreferrer"
+        >
+          <button type="button">Login</button>
+        </a>
+      </div>
+    </div>
+  );
+};
+
 export default function App() {
   return (
     <Router>
       <Switch>
-        <Route path="/" component={Hello} />
+      <Route path="/" component={Hello} />
+      <Route path="/welcome" component={Welcome} />
       </Switch>
     </Router>
   );
